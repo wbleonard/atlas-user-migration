2d8b8d6e57b8b9056c595bbc98bd651f561659f7
Brian Leonard <brian.leonard@mongodb.com>
2019-01-23 17:28:18 -0500

    params.py added

---
+++ params.py
@@ -0,0 +1,11 @@
+# Input parameters
+
+# Source DB
+source_conn_string = <Application connection string>
+source_database = 'admin'
+
+# For Atlas REST API
+target_project_id = '<Project ID found under Atlas Project Settings>'
+target_api_user = '<User Name>'
+target_api_key = '<API Key>'
+
